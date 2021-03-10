8f5678c66ac657b196b13cd269af244987530308
TerpstraS <>
2021-03-10 12:56:56 +0100

    fixed performance issues

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy as sp
+import scipy.linalg as sp_lin
+import numba
 
-
+# @numba.njit
 def construct_M(N_x, N_y):
 
-    M = np.zeros((N_x*N_y, N_x*N_y), dtype=int)
+    M = np.zeros((N_x*N_y, N_x*N_y))
 
     # loop through each grid point
     for i in range(N_x * N_y):
@@ -15,40 +16,51 @@ def construct_M(N_x, N_y):
         y_grid = i // N_x
 
         # determine neighbours and check if they are in the boundaries of the system
-        neighbours_candidates = [[1, 0], [-1, 0], [0, -1], [0, 1]]
-        neighbours = []
-        for neighbours_candidate in neighbours_candidates:
+        neighbours_candidates = np.array([[1, 0], [-1, 0], [0, -1], [0, 1]])
+        neighbours = np.full((4, 2), -1)
+        for k, neighbours_candidate in enumerate(neighbours_candidates):
             if neighbours_candidate[0] + x_grid >= 0 and neighbours_candidate[0] + x_grid < N_x \
                 and neighbours_candidate[1] + y_grid >= 0 and neighbours_candidate[1] + y_grid < N_y:
-                neighbours.append([neighbours_candidate[0] + x_grid, neighbours_candidate[1] + y_grid])
 
-        # loop through i-th row of M
-        for j in range(N_x * N_y):
+                neighbours[k, 0] = neighbours_candidate[0] + x_grid
+                neighbours[k, 1] = neighbours_candidate[1] + y_grid
 
-            # place diagonal
-            if i == j:
-                M[i, j] = -len(neighbours)
-            else:
-                for neighbour in neighbours:
-                    x_neighbour = neighbour[0]
-                    y_neighbour = neighbour[1]
+        n_neighbours = 0
+        for neighbour in neighbours:
+            x_neighbour = neighbour[0]
+            y_neighbour = neighbour[1]
 
-                    x_M = x_neighbour + y_neighbour * N_x
-                    y_M = i
+            if x_neighbour != -1 and y_neighbour != -1:
+                n_neighbours += 1
+                x_M = x_neighbour + y_neighbour * N_x
+                y_M = i
+                M[x_M, y_M] = 1
 
-                    M[x_M, y_M] = 1
+        M[i, i] = -n_neighbours
 
     return M
 
 
 def main():
 
-    N_x = 3
-    N_y = 3
+    L = 1
+    N = 50
+
+    N_x = N * L
+    N_y = N * L
     M = construct_M(N_x, N_y)
 
     print(M)
 
+    # w, v = sp_lin.eigh(M)
+    #
+    # plt.figure()
+    # for v_i in v:
+    #     plt.plot(v_i)
+    # plt.show()
+    #
+    # print(w)
+    # print(v)
     return
 
 
