32330a39355547ea67741709c03d2f9fd30a8c3b
TerpstraS <>
2021-03-13 19:28:40 +0100

    leapfrog all necessary parts except extra bonus RK45

--- leapfrog.py
+++ leapfrog.py
@@ -1,27 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import numba
 
 
-def main():
-
-    dt = 0.001
-    m = 1
-    k = 0.05
-
-    time_total = 50
+@numba.njit
+def leapfrog(time_total, dt, k, f_t=False, omega=0.01, m=1):
     time_arr = np.zeros(int(time_total / dt))
-    x = np.zeros(int(time_total / dt), dtype=float)
-    v = np.zeros(int(time_total / dt), dtype=float)
+    x = np.zeros(int(time_total / dt))
+    v = np.zeros(int(time_total / dt))
     x[0] = 1
     v[0] = 0
 
     for t in range(1, int(time_total / dt)):
         x[t] = dt * v[t-1] + x[t-1]
-        v[t] = dt * (-k * x[t]/m) + v[t-1]
+        if f_t:
+            v[t] = dt * (np.sin(omega * x[t]) - k * x[t]) / m + v[t-1]
+        else:
+            v[t] = dt * (-k * x[t]/m) + v[t-1]
         time_arr[t] = t * dt
+
+    return time_arr, x, v
+
+
+def leapfrog_hooke():
+    dt = 0.001
+    m = 1
+    k = 0.05
+    time_total = 50
+
+    time_arr, x, v = leapfrog(time_total, dt, k, f_t=False, m=m)
+
+    plt.figure()
+    plt.plot(v, x)
 
     plt.rcParams.update({"font.size": 14})
     fig, ax1 = plt.subplots()
+    plt.title("Hooke")
     color = "black"
     ax1.plot(time_arr, x, color=color)
     ax1.set_xlabel("time (s)")
@@ -33,7 +47,45 @@ def main():
     ax2.set_ylabel("v (m/s)", color=color)
     ax2.tick_params(axis="y", labelcolor=color)
     fig.tight_layout()
-    plt.savefig("./results/leapfrog/x_v_k{}.png".format(k))
+    # plt.savefig("./results/leapfrog/x_v_k{}.png".format(k))
+
+
+def leapfrog_sinusiodal():
+
+    dt = 0.001
+    m = 1
+    k = 0.3
+    time_total = 100
+    omega = 0.1
+
+    time_arr, x, v = leapfrog(time_total, dt, k, f_t=True, omega=omega, m=m)
+    #
+    plt.figure()
+    plt.plot(v, x)
+
+    plt.rcParams.update({"font.size": 14})
+    fig, ax1 = plt.subplots()
+    plt.title("Omega")
+    color = "black"
+    ax1.plot(time_arr, x, color=color)
+    ax1.set_xlabel("time (s)")
+    ax1.set_ylabel("x (m)", color=color)
+    ax1.tick_params(axis="y", labelcolor=color)
+    ax2 = ax1.twinx()
+    color = "blue"
+    ax2.plot(time_arr, v, color=color)
+    ax2.set_ylabel("v (m/s)", color=color)
+    ax2.tick_params(axis="y", labelcolor=color)
+    fig.tight_layout()
+    # plt.savefig("./results/leapfrog/sinusiodal_x_v_k{}.png".format(k))
+
+
+def main():
+
+    leapfrog_hooke()
+
+    leapfrog_sinusiodal()
+
     plt.show()
 
     return
