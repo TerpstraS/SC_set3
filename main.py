60b7f2ce36290f9981e0c8c8048d6ec238a91779
TerpstraS <>
2021-03-15 16:42:03 +0100

    fixed drum program

--- main.py
+++ main.py
@@ -4,47 +4,12 @@ import scipy.linalg as sp_lin
 import scipy.sparse.linalg as sp_lin_sparse
 import time
 
-# def construct_M(N_x, N_y, circular=False):
-#
-#     # add 2 to each side for the boundary conditions
-#     M = np.zeros(((N_x + 2) * (N_y + 2), (N_x + 2) * (N_y + 2)), dtype=int)
-#
-#     # loop through each grid point
-#     for i in range((N_x + 2) * (N_y + 2)):
-#
-#         # skip boundaries (where the value should be kept 0, since fixed bounday conditions)
-#         if i < N_x + 2 or i >= (N_x + 2) * (N_y + 2) - (N_x + 2) or i % (N_x + 2) == 0 or i % (N_x + 2) == N_x + 1:
-#             continue
-#
-#         # calculate x- and y-coordinate for each grid point
-#         x_grid = i % (N_x + 2)
-#         y_grid = i // (N_x + 2)
-#
-#         # determine neighbours and check if they are in the boundaries of the system
-#         neighbours_candidates = np.array([[1, 0], [-1, 0], [0, -1], [0, 1]])
-#         neighbours = np.full((4, 2), -1)
-#         for k, neighbours_candidate in enumerate(neighbours_candidates):
-#             if neighbours_candidate[0] + x_grid >= 0 and neighbours_candidate[0] + x_grid < N_x + 2 \
-#                 and neighbours_candidate[1] + y_grid >= 0 and neighbours_candidate[1] + y_grid < N_y + 2:
-#
-#                 neighbours[k, 0] = neighbours_candidate[0] + x_grid
-#                 neighbours[k, 1] = neighbours_candidate[1] + y_grid
-#
-#         for neighbour in neighbours:
-#             x_neighbour = neighbour[0]
-#             y_neighbour = neighbour[1]
-#
-#             if x_neighbour != -1 and y_neighbour != -1:
-#                 x_M = x_neighbour + y_neighbour * (N_x + 2)
-#                 y_M = i
-#                 M[x_M, y_M] = 1
-#
-#         # set diagional to -4
-#         M[i, i] = -4
-#
-#     return M
 
-def construct_M(N_x, N_y, circular=False):
+def construct_M(L, N_x, N_y, circular=False):
+    """ This is a function that makes the matrix of a rectangle. """
+
+    len_M = N_x * N_y
+    M = np.zeros((len_M, len_M))
 
     if circular:
         assert N_x == N_y, "grid should be square for a circular domain"
@@ -52,59 +17,63 @@ def construct_M(N_x, N_y, circular=False):
         x_center, y_center = N_x / 2, N_y / 2
         r = N_x / 2
 
-    M = np.zeros(((N_x) * (N_y), (N_x) * (N_y)), dtype=int)
+    # index in M of diagonals of 1. Large is the outer diagonal, small is the diagonal hugging
+    # the true matrix diagonal
+    M_large_diag = N_x
+    M_small_diag = 1
 
-    # loop through each grid point
-    for i in range(N_x * N_y):
+    for i in range(len_M):
 
-        # calculate x- and y-coordinate for each grid point
-        x_grid = i % N_x
-        y_grid = i // N_x
-
-        # skip if grid point lays outside circle
-        if circular and np.sqrt((x_center - x_grid)**2 + (y_center - y_grid)**2) > r:
-            continue
-
-        # determine neighbours and check if they are in the boundaries of the system
-        neighbours_candidates = np.array([[1, 0], [-1, 0], [0, -1], [0, 1]])
-        neighbours = np.full((4, 2), -1)
-        for k, neighbours_candidate in enumerate(neighbours_candidates):
-            if neighbours_candidate[0] + x_grid >= 0 and neighbours_candidate[0] + x_grid < N_x \
-                and neighbours_candidate[1] + y_grid >= 0 and neighbours_candidate[1] + y_grid < N_y:
-
-                neighbours[k, 0] = neighbours_candidate[0] + x_grid
-                neighbours[k, 1] = neighbours_candidate[1] + y_grid
-
-        n_neighbours = 0
-        for neighbour in neighbours:
-            x_neighbour = neighbour[0]
-            y_neighbour = neighbour[1]
-
-            if x_neighbour != -1 and y_neighbour != -1:
-                n_neighbours += 1
-                x_M = x_neighbour + y_neighbour * N_x
-                y_M = i
-                M[x_M, y_M] = 1
-
+        # set values at diagonal to -4
         M[i, i] = -4
 
-    return M
+        # fill in outermost diagonals
+        if M_large_diag >= 0 and M_large_diag < len_M:
+            M[i, M_large_diag] = 1
+            M[M_large_diag, i] = 1
+
+        # fill in the diagonal hugging the true diagonal
+        if not M_small_diag % N_x == 0:
+            if M_small_diag >= 0 and M_small_diag < len_M:
+                M[i, M_small_diag] = 1
+                M[M_small_diag, i] = 1
+
+        M_large_diag += 1
+        M_small_diag += 1
+
+    # step over matrix, check if the column is in the circle
+    if circular:
+        for i in range(len_M):
+
+            # true grid coordinates
+            x_grid = i % N_x
+            y_grid = i // N_x
+
+            # check if grid point lays outside circle
+            if np.sqrt((x_center - x_grid)**2 + (y_center - y_grid)**2) > r:
+
+                # set column to zerp (except for the diagonal)
+                for j in range(len_M):
+                    if not i == j:
+                        M[i, j] = 0
+
+    # multiply M by dx
+    return M * (1/(L/N_x)**2)
 
 
 def solve_eigen_problem(L, N_x, N_y, circular=False, sparse=True, k=6):
 
-    M = construct_M(N_x, N_y, circular=circular)
-    M = -M / (L /N_x**2)   # apply division by dx^2
+    M = construct_M(L, N_x, N_y, circular)
 
     print("Solving eigenvalues...")
 
     if sparse:
-        eig_vals, eig_vecs = sp_lin_sparse.eigs(M, k)
+        eig_vals, eig_vecs = sp_lin_sparse.eigs(M, k=k, which="SM")     # or which="LR"
     else:
-        eig_vals, eig_vecs = sp_lin.eigh(M)
+        eig_vals, eig_vecs = sp_lin.eig(M)
 
     eig_vals = eig_vals.real
-    eig_vecs = eig_vecs.T
+    eig_vecs = eig_vecs.real.T
 
     print("Eigenvalues found...\n")
 
@@ -112,7 +81,7 @@ def solve_eigen_problem(L, N_x, N_y, circular=False, sparse=True, k=6):
 
 
 def time_func(t, labda):
-    A, B = 1, 0
+    A, B = 1, 1
     return A * np.cos(labda*t) + B * np.sin(labda*t)
 
 
@@ -122,48 +91,50 @@ def main():
 
     L = 1
     N = 30
+    sparse = True
+    k = 3
 
     N_x = N * L
     N_y = N * L
 
-    eig_vals_square, eig_vecs_square = solve_eigen_problem(L, N*L, N*L, sparse=False, k=2)
-    eig_vals_rect, eig_vecs_rect = solve_eigen_problem(L, N*L, N*2*L, sparse=False, k=2)
-
-    # circle doesn't properly work
-    eig_vals_circle, eig_vecs_circle = solve_eigen_problem(L, N*L, N*L, sparse=False, circular=True, k=2)
+    eig_vals_square, eig_vecs_square = solve_eigen_problem(L, N*L, N*L, sparse=sparse, k=k)
+    eig_vals_rect, eig_vecs_rect = solve_eigen_problem(L, N*L, N*2*L, sparse=sparse, k=k)
+    eig_vals_circle, eig_vecs_circle = solve_eigen_problem(L, N*L, N*L, sparse=sparse, circular=True, k=k)
 
     print("Total time: {:.2f} seconds".format(time.time()-time_start))
 
-    u = eig_vecs_square[0].reshape((N*L, N*L)).T
-    labda = np.sqrt(eig_vals_square[0])
+    # animation
+    ## TODO: add (fixed?) colorbar
+    u = eig_vecs_circle[0].reshape((N*L, N*L)).T
+    labda = np.sqrt(-eig_vals_circle[0])
     fig, ax = plt.subplots()
     plt.xlabel("x")
     plt.ylabel("y")
     u_t = u * time_func(0, labda)
     ax.matshow(u_t, origin="lower")
-
-    for t in np.arange(0, 0.5*np.pi, 0.01):
+    for t in np.arange(0, 0.3*np.pi, 0.01):
         u_t = u * time_func(t, labda)
         plt.cla()
         ax.matshow(u_t, origin="lower")
         plt.title("$t = {:.2f}$ seconds".format(t))
         plt.pause(0.001)
 
-    for i, eig_vec in enumerate(eig_vecs_square[:2]):
+
+    for i, eig_vec in enumerate(eig_vecs_square[:k]):
         plt.matshow(eig_vec.reshape((N*L, N*L)).T, origin="lower")
         plt.title("Square drum: eigenvalue {:.0f}".format(eig_vals_square[i]))
         plt.xlabel("x")
         plt.ylabel("y")
         plt.colorbar()
 
-    for i, eig_vec in enumerate(eig_vecs_rect[:2]):
+    for i, eig_vec in enumerate(eig_vecs_rect[:k]):
         plt.matshow(eig_vec.reshape(N*L, N*2*L).T, origin="lower")
         plt.title("Rectangular drum: eigenvalue {:.0f}".format(eig_vals_rect[i]))
         plt.xlabel("x")
         plt.ylabel("y")
         plt.colorbar()
 
-    for i, eig_vec in enumerate(eig_vecs_circle[:2]):
+    for i, eig_vec in enumerate(eig_vecs_circle[:k]):
         plt.matshow(eig_vec.reshape(N*L, N*L).T, origin="lower")
         plt.title("Circular drum: eigenvalue {:.0f}".format(eig_vals_circle[i]))
         plt.xlabel("x")
